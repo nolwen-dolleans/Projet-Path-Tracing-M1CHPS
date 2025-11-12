f1ccc3314d6aba135ff3bc224832af0bfb4bc572
langhold <nolwen.dolleans@gmail.com>
2025-11-12 23:03:41 +0100

    commit initial pour BRDF

---
+++ src/BRDF/BRDF.c
@@ -0,0 +1,8 @@
+//
+//  BRDF.c
+//  Projet PPN
+//
+//  Created by Nolwen Dolléans on 12/11/2025.
+//
+
+#include "BRDF.h"
---
+++ src/BRDF/BRDF.h
@@ -0,0 +1,13 @@
+//
+//  BRDF.h
+//  Projet PPN
+//
+//  Created by Nolwen Dolléans on 12/11/2025.
+//
+
+#ifndef BRDF_h
+#define BRDF_h
+
+#include <stdio.h>
+
+#endif /* BRDF_h */
