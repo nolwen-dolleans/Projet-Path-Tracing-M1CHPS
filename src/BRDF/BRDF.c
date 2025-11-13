4eaa4f90d497d00d2884d4d65ad3d63243aea9c8
langhold <nolwen.dolleans@gmail.com>
2025-11-13 02:26:13 +0100

    Implementation du ray_sampling basique

--- src/BRDF/BRDF.c
+++ src/BRDF/BRDF.c
@@ -9,13 +9,22 @@
 #include "BRDF.h"
 
 
-uint24_t * ray_sampling(struct * vector e, const Ray * r, const struct Scene * S, const int d, const int dmax){
-	uint24_t  * color = malloc(uint8_t*3);
+uint24_t * ray_sampling(Vector * eye, Ray * r, Scene * S, int d, int dmax){
+	uint24_t  * color = malloc_check(sizeof(uint8_t) *3);
+	struct Vector * O = create_vector_default();
 	
 	if (d == dmax) {
 		set_color_24bit(color,0,0,0); 			// par défaut met en noir si le rayon est hors-limite
 		return color;
 	}
-	e = get_intersection(e, r, S);				// par défaut met en noir si le rayon est hors-limite
+	O = get_intersection(eye,r,S);				// O le point d'intersection du rayon sur l'objet et i le type d'objet rencontré
+	
+	struct Ray * r_new = random_Ray(O);
+	uint24_t  * color_i = ray_sampling(O,r_new,S,d+1,dmax);
+	
+	color->byte[0] = color->byte[0] + color_i->byte[0] * BRDF(r, r_new, 1)/2/M_PI; 	// 1 représente une sphère
+	color->byte[1] = color->byte[1] + color_i->byte[1] * BRDF(r, r_new, 1)/2/M_PI; 	// 1 représente une sphère
+	color->byte[2] = color->byte[2] + color_i->byte[2] * BRDF(r, r_new, 1)/2/M_PI; 	// 1 représente une sphère
+	return color;
 }
 
--- src/BRDF/BRDF.h
+++ src/BRDF/BRDF.h
@@ -12,28 +12,39 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#include <math.h>
 #include "../image/image.h"
 #include "../ray/ray.h"
 #include "../vector/vector.h"
-#include "../Scene/Scene.h"
+#include "../tools/tools.h"
 
-/**
- * @brief  list of all objects of the scene.
- * @param Objects array of objects
- */
 
-extern Vector* get_intersection(const Ray* const r, Object* O);
+typedef struct Scene{}Scene;  //will be put inside of scene.h
+typedef struct Object{}Object;
+
+
+extern Vector* get_intersection(Vector * position,Ray* r,Scene * S);
 
 /**
  * @brief  recursive path-tracing algorithm to compute a single sample with Lambertian materials.
- * @param e origine of the incomming ray
+ * @param eye origine of the incomming ray
  * @param r incident ray
  * @param S pointer of the current scene
  * @param d current number of bounces
  * @param dmax maximum bounces number
  * @return color of the pixels at the e object
  */
-uint24_t * ray_sampling(const struct vector e, const Ray * r, const struct Scene * S, const int d, const int dmax);
+uint24_t * ray_sampling(Vector * eye, Ray * r, Scene * S, int d, int dmax);
 
+
+/**
+ * @brief make a random ray emeted in the point origin
+ * @param origin origin of the emeted ray
+ * @return Ray emited
+ */
+struct Ray * random_Ray(Vector * origin);
+
+
+float BRDF(Ray * r_i, Ray * r_e, int object);
 
 #endif /* BRDF_h */
