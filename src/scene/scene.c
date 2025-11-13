95e30a1c71e8fd1c1668e976ea404d80dd6ebd99
langhold <nolwen.dolleans@gmail.com>
2025-11-13 16:19:21 +0100

    fichiers sources créés

---
+++ src/scene/scene.c
@@ -0,0 +1,8 @@
+//
+//  scene.c
+//  Projet PPN
+//
+//  Created by Nolwen Dolléans on 13/11/2025.
+//
+
+#include "scene.h"
---
+++ src/scene/scene.h
@@ -0,0 +1,21 @@
+//
+//  scene.h
+//  Projet PPN
+//
+//  Created by Nolwen Dolléans on 13/11/2025.
+//
+
+#ifndef scene_h
+#define scene_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "../tools/tools.h"
+#include "../vector/vector.h"
+#include "../ray/ray.h"
+
+#endif /* scene_h */
