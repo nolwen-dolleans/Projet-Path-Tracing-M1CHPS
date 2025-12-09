81c3c1e5e377e0f28cf6dbcbef2b3187672e6e74
langhold <nolwen.dolleans@gmail.com>
2025-12-09 18:32:28 +0100

    corrections mineures

--- performance/run_24_32_bit.sh
+++ performance/run_24_32_bit.sh
@@ -5,6 +5,10 @@ WIDTH=500
 HEIGHT=500
 SAMPLES=200
 
+if [ ! -e "measures" ]
+then
+mkdir measures
+fi
 
 for IMGSIZE in "24" "24ptr" "32";do
 cd ..
@@ -13,6 +17,13 @@ START=`date +%s%N`
 END=`date +%s%N`
 cd performance
 EXE_TIME=$(($END - $START))
+
+
+
+if [ -e "measures/time_$IMGSIZE _WIDTH=$WIDTH HEIGHT=$HEIGHT SAMPLES=$SAMPLES" ]
+then
+rm -rf "measures/time_$IMGSIZE _WIDTH=$WIDTH HEIGHT=$HEIGHT SAMPLES=$SAMPLES"
+fi
 
 echo "Time (s)\n $EXE_TIME" >> "measures/time_$IMGSIZE _WIDTH=$WIDTH HEIGHT=$HEIGHT SAMPLES=$SAMPLES"
 
