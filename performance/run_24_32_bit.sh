30a6793b9e5e5744fa66a06b2bce61d1c235022c
langhold <nolwen.dolleans@gmail.com>
2025-12-09 17:22:37 +0100

    inplementation d un premier protocole de performances

--- performance/run_24_32_bit.sh
+++ performance/run_24_32_bit.sh
@@ -3,11 +3,18 @@
 
 WIDTH=500
 HEIGHT=500
-SAMPLES=1000
+SAMPLES=200
 
 
 for IMGSIZE in "24" "24ptr" "32";do
+cd ..
+START=`date +%s%N`
+./build/ppm $IMGSIZE $WIDTH $HEIGHT $SAMPLES
+END=`date +%s%N`
+cd performance
+EXE_TIME=$(($END - $START))
 
-./../build/ppm $IMGSIZE $WIDTH $HEIGHT $SAMPLES
+echo "Time (s)\n $EXE_TIME" >> "measures/time_$IMGSIZE _WIDTH=$WIDTH HEIGHT=$HEIGHT SAMPLES=$SAMPLES"
 
 done
+rm -rf tmp.csv
